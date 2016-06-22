a50838af868d215bc84eac062ed91430ea9a4d12
TanmayParekh <tparekh97@gmail.com>
2016-06-22 11:06:15 +0530

    Uploading Base Project
    Tank can move, shoot.

---
+++ Assets/scripts/TankMove.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TankMove : MonoBehaviour {
+
+    public float speed = 1.0f;          // Speed at which the tank moves.
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        // WASD Movement
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += Vector3.up * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += Vector3.down * speed * Time.deltaTime;
+        }
+
+        // MOUSE Movement
+        Vector3 mouse_pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.rotation = Quaternion.LookRotation(Vector3.forward, mouse_pos - transform.position) * Quaternion.Euler(0,0,90);
+
+    }
+}
---
+++ Assets/scripts/bulletController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bulletController : MonoBehaviour {
+
+    public GameObject parent;           // The tank which has fired this bullet
+    public float damage;                // Damage that this bullet can do
+    public float radius;                // Radius of the bullet
+
+    private float prevScale;
+
+    // Use this for initialization
+    void Start ()
+    {
+        // Change the radius to the given radius
+        gameObject.transform.localScale = new Vector3(1,1,1) * radius;
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        // If the colliding gameobject is not the tank firing it, then destroy bullet.
+        // We can add damage to the colliding tank here.
+        if (coll.gameObject.name != parent.name)
+        {
+            Destroy(gameObject);
+        }       
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
